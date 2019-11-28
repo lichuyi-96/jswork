e00e7b9bc8e29fbdb95dca1154261bad34679770
lichuyi-96 <1691684771@qq.com>
2019-11-28 15:54:53 +0800

    work13

--- work12-1/index1.js
+++ work12-1/index1.js
@@ -5,12 +5,13 @@ function setCalendar(year){
     var year = parseInt(year)
     var pre = year-1
     var next = year+1
-    console.log(pre)
-    console.log(next)
+    // console.log(year)
+    // console.log(pre)
+    // console.log(next)
     document.getElementById('curyear').setAttribute('data-year',year);
     document.getElementById('preyear').setAttribute('data-year',pre);
     document.getElementById('nextyear').setAttribute('data-year',next);
-    document.getElementById('curyear').innerText = year1
+    document.getElementById('curyear').innerText = year
     document.getElementById('preyear').innerText =  "<<"+pre
     document.getElementById('nextyear').innerText = next+  ">>"
     document.getElementById("cal").innerHTML=calender(year)
