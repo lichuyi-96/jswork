3d66eb07c3c255c039d738e08fade1dda066c491
lichuyi-96 <1691684771@qq.com>
2019-12-17 17:58:19 +0800

    work17

--- work17/index.js
+++ work17/index.js
@@ -1,6 +1,5 @@
-import { tmpdir } from "os";
 
-var inputs = document.getElementsByClassName('input');
+var inputs = document.getElementsByTagName('input');
 
 for(var i =0;i < inputs.length - 1;++i){
     inputs[i].onblur = inputBlur;
@@ -18,14 +17,14 @@ function inputBlur() {
         return false;
     }
 
-    var reg_mag = getRegMsg(name,tips);
+    var reg_msg = getRegMsg(name,tips);
 
-    if (reg_mag['reg'].test(val)){
+    if (reg_msg['reg'].test(val)){
 
-        success(tips_obj,reg_mag['msg']['success']);
+        success(tips_obj,reg_msg['msg']['success']);
     }else{
 
-        error(tips_obj,reg_mag['msg']['error']);
+        error(tips_obj,reg_msg['msg']['error']);
     }
 }
 function getRegMsg(name,tips) {
@@ -35,17 +34,33 @@ function getRegMsg(name,tips) {
             reg = /^[a-zA-Z]{4,12}$/;
             msg = {'success': '用户名输入正确','error': tips};
             break;
+            case 'pwd':
+                reg = /^\w{6,20}$/;
+                msg = {'success': '密码输入正确','error': tips};
+                break;
             case 'repwd':
                 var con = document.getElementsByTagName('input')[1].value;
                 reg = RegExp("^" + con + "$");
                 msg = {'success': '两次密码输入正确', 'error': '两次输入密码不一致'};
                 break;
                 case 'tel':
-                    reg = /^1[24578]\d{9}$/;
+                    reg = /^1[34578]\d{9}$/;
                     msg = {'success':'手机号码输入正确','error': tips};
                     break;
-                    case 'tel':
-                        reg  = /^1[34578]\d{9}$/;
+                    case 'email':
+                        reg  = /^(\w+(\_|\-|\.)*)+@(\w+(\-)?)+(\.\w{2,})+$/
                         msg  = {'success': '邮箱输入正确', 'error':tips};
+                        break;
     }
+    return{'reg': reg, 'msg':msg};
+}
+
+function success(obj,msg){
+    obj.className = 'success';
+    obj.innerHTML = msg;
+}
+
+function error(obj,msg) {
+    obj.className = 'error';
+    obj.innerHTML = msg + ',请重新输入';
 }
