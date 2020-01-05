24f7e1bc8fdce04c19e66e2db2d4214afe920cda
lichuyi-96 <1691684771@qq.com>
2020-01-05 13:01:22 +0800

    work23

--- work22/index.js
+++ work22/index.js
@@ -1,77 +1,77 @@
-var jms = null;
+jms = null
 var timeHandle = null;
-var xmlhttp= new XMLHttpRequest()
-xmlhttp.onreadystatechange = function(){
-    if(xmlhttp.readyState == 4 && xmlhttp.status ==200){
+var xmlhttp=new XMLHttpRequest()
+xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState = 4 && xmlhttp.status == 200) {
         try{
-            let ajaxData=JSON.parse(xmlhttp.responseText)
+            let ajaxData = JSON.parse(xmlhttp.responseText)
             result = ajaxData.map((val,index)=>{return index+1+":"+val.name+"--"+val.score})
             result = '排行榜\n'+result.join(String.fromCharCode(13))
-            alert(result)         
+            alert(result)
         }catch(e){
             console.log(e)
         }
     }
 }
-window.onload = function(){
+window.onload = function () {
     var radios = document.getElementsByName("level");
-    for(var i = 0, j= radios.length;i<j;i++){
-        radios[i].onclick = function(){
-            if(jms != null)
-               if(jms.landMineCount>0)
-                     if(!confirm("确定结束当前游戏？"))
-                         return false;
+    for (var i = 0,j = radios.length; i < j; i++) {
+        radios[i].onclick = function () {
+            if (jms != null)
+                if (jms.landMineCount > 0)
+                    if(!confirm("确定结束当前游戏？"))
+                        return false;
             var value = this.value;
-           init(value,value,value*value/5-value,value*value/5);
-           document.getElementById("Jms_main").style.width  = value*48+180+60+'px';
+            init(value, value, value * value / 5 - value, value * value / 5);
+            document.getElementById("JMS_main").style.width = value * 40 + 180 + 60 + "px";
         }
     }
-    init(10,10);
+    init(10, 10);
 };
 
-function init(rowCount,colCount,minLandMineCount,maxLandMineCount){
+function init(rowCount, colCount, minLandMineCount, maxLandMineCount) {
     var doc = document,
-    landMineCountElement = doc.getElementById("landMineCount"),
-    timeShow = doc.getElementById("cosTime"),
-    beginButton = doc.getElementById("begin");
-    if(jms != null){
+        landMineCountElement = doc.getElementById("landMineCount"),
+        timeShow = doc.getElementById("costTime"),
+        beginButton = doc.getElementById("begin");
+    if (jms != null) {
         clearInterval(timeHandle);
         timeShow.innerHTML = 0;
         landMineCountElement.innerHTML = 0;
     }
-    jms = JMS("landmine",rowCount,colCount,mianLandMinCount,maxLandMineCount);
-    jms.endCallBack = function(status){
+    jms = JMS("landmine", rowCount, colCount, minLandMineCount, maxLandMineCount);
+    jms.endCallBack = function (status) {
         clearInterval(timeHandle);
         document.getElementById("begin").disabled=""
         if(status){
-            let name = document.getElementById("name").innerText
-            let score= document.getElementById("costTime").innerText
-            xmlhttp.open('post','http://139.9.81.203:8090/gameRecord/mine',true)
-            xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-            xmlhttp.send("name="+name+'&score='+score+"&order=1")
+            let name = document.getElementById('name').innerText
+            let score = document.getElementById('costTime').innerText
+            xmlhttp.open('post', 'http://139.9.81.203:8090/gameRecord/mine', true)
+            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xmlhttp.send('name=' + name + '&score=' + score + "&order=1")
         }else{
-            alert('扫雷失败')
+            alert('扫雷失败！')
         }
 
     };
-    jms.landMineCountBack = function(count){
+    jms.landMineCallBack = function (count) {
         landMineCountElement.innerHTML = count;
     };
 
-
-    beginButton.click = function(){
+   
+    beginButton.onclick = function () {
         jms.play();
 
-
+      
         landMineCountElement.innerHTML = jms.landMineCount;
 
-
+       
         jms.begin();
         this.disabled="disabled"
 
-
-        timeHandle = setInterval(function(){
-            timeShow.innerHTML = parseInt((new Date()-jms.beginTime/1000);
-        },1000);
+      
+        timeHandle = setInterval(function() {
+            timeShow.innerHTML = parseInt((new Date() - jms.beginTime) / 1000);
+        }, 1000);
     };
 }
